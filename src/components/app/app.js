4e304d1e5acd3bad6123bd20545254fc0df98d8d
Igor Podgorniy <ipod-92@mail.ru>
2021-10-28 20:52:20 +0300

    Добавил функционал фильтра.

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -17,13 +17,15 @@ export default class App extends Component {
                 { label: "Theat is so good", important: false, like: false, id: 'sdfsg' },
                 { label: "I need a break...", important: false, like: false, id: 'dsjnvj' }
             ],
-            term: ''
+            term: '',
+            filter: 'all'
         };
         this.deleteItem = this.deleteItem.bind(this);
         this.addItem = this.addItem.bind(this);
         this.onToggleImportant = this.onToggleImportant.bind(this);
         this.onToggleLiked = this.onToggleLiked.bind(this);
         this.onUpdateSearch = this.onUpdateSearch.bind(this);
+        this.onFilterSelect = this.onFilterSelect.bind(this);
 
         this.maxId = 4;
     }
@@ -88,16 +90,28 @@ export default class App extends Component {
         })
     }
 
+    filterPost(items, filter) {
+        if(filter === "like") {
+            return items.filter(item => item.like);
+        } else {
+            return items;
+        }
+    }
+
     onUpdateSearch(term) {
         return this.setState({term});
     }
 
+    onFilterSelect(filter) {
+        return this.setState({filter})
+    }
+
     render() {
-        const {data, term} = this.state;
+        const {data, term, filter} = this.state;
         const likedPosts = data.filter(item => item.like).length;
         const allPosts = data.length;
 
-        const visiblePosts = this.searchPost(data, term);
+        const visiblePosts = this.filterPost(this.searchPost(data, term), filter);
 
         return (
             <div className="app">
@@ -109,7 +123,10 @@ export default class App extends Component {
                     <SearchPanel
                         onUpdateSearch={this.onUpdateSearch}
                     />
-                    <PostStatusFilter/>
+                    <PostStatusFilter
+                        filter={filter}
+                        onFilterSelect={this.onFilterSelect}
+                    />
                 </div>
                 <PostList
                     posts={visiblePosts}
